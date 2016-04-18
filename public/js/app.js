b41a465eb678375e3f6d6ae2aac9a049f7a443c5
Apone <soyapone@gmail.com>
2016-04-18 20:24:10 +0200

    Añadido el servicio HTTP de angular para hacer peticiones

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,31 @@
     this.products = gems;
   });
 
-  app.controller('CalculoController', function(){
-    this.calculo = {};
+  app.controller('CalculoController', [ '$http', '$log', function($http,$log){
+    var cal = this;
 
     this.sendCalculo = function(){
       // mandar hacer el cálculo
-      alert("Alerta me han mandado un calculo!");
-      this.calculo = {};
+      //alert("Alerta me han mandado un calculo!");
+      //this.calculo.resultado = "";
+      var myurl = '../JSON/calculo2?var1='+cal.calculo.operando1+"&var2="+cal.calculo.operando2;
+      $http({
+        method: 'GET',
+        url: myurl
+      }).then(function successCallback(response) {
+          // this callback will be called asynchronously
+          // when the response is available
+          // Aquí no podemos poner THIS!!
+          cal.calculo.resultado = response.data.result;
+        }, function errorCallback(response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+          // Aquí no podemos poner THIS!!
+          cal.calculo.resultado = 'There´s an error connecting with the server.';
+        });
+
     };
-  });
+  }]);
 
 
 
