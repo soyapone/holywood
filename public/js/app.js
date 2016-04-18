29704a35d24b59533ebd3bdcf8c698e043e10801
Apone <soyapone@gmail.com>
2016-04-18 13:41:38 +0200

    Añadidas más funcionalidades de Angular.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,36 @@
 (function(){
-  var app = angular.module('myCalculo', [ ]);
+  var app = angular.module('myApp', [ ]);
 
-  app.controller('CalculoController', function(){
-    this.product = gem;
+  app.controller('InicializadorController', function(){
+    this.products = gems;
   });
 
-var gem = {
+  app.controller('CalculoController', function(){
+    this.calculo = {};
+
+    this.sendCalculo = function(){
+      // mandar hacer el cálculo
+      alert("Alerta me han mandado un calculo!");
+      this.calculo = {};
+    };
+  });
+
+
+
+var gems = [
+{
   name: 'Dodecaedro',
   price: 2.95,
-  description: 'esta es la descripción'
+  description: 'esta es la descripcion',
+  canPurchase: false
+},
+{
+  name: 'Gema superchula',
+  price: 17.00,
+  description: 'Gema que te cagas de chuli',
+  canPurchase: true
 }
+
+]
 
 })();
