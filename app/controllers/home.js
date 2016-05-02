4abe706e9f03ba3651ae041cae0eef1b200570ef
Apone <soyapone@gmail.com>
2016-05-02 19:48:36 +0200

    Cambiado el scope de la autentificación por Facebook

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -48,7 +48,9 @@ app.get('/logout', function(req,res){
   // Redirect the user to Facebook for authentication.  When complete,
   // Facebook will redirect the user back to the application at
   //     /auth/facebook/callback
-  app.get('/auth/facebook', passport.authenticate('facebook', { scope: [ 'email' ] }) );
+  app.get('/auth/facebook',
+    passport.authenticate('facebook', { scope: [ 'email' ] })
+);
 
   // Facebook will redirect the user to this URL after approval.  Finish the
   // authentication process by attempting to obtain an access token.  If
