2ef61c38d786ddae706c047bed5917f3da0ae599
Diego Martín <soyapone@gmail.com>
2017-05-24 17:53:13 +0200

    Para pasear la IP y mostrar la IPv6 e IPv4 correctamente.
    Hay que agregar el paquete "ipaddr.js"

--- app/statics/APIRequests_db.js
+++ app/statics/APIRequests_db.js
@@ -1,13 +1,36 @@
 
 var mongoose = require('mongoose');
 var APIRequest = mongoose.model('APIRequest');
+var ipaddr = require('ipaddr.js');
 
 
+function transformIP(ipString){
+
+  if (ipaddr.IPv4.isValid(ipString)) {
+    // ipString is IPv4
+    return ipString;
+  } else if (ipaddr.IPv6.isValid(ipString)) {
+    var ip = ipaddr.IPv6.parse(ipString);
+    if (ip.isIPv4MappedAddress()) {
+      // ip.toIPv4Address().toString() is IPv4
+      return ip.toIPv4Address().toString();
+    } else {
+      // ipString is IPv6
+      return ipString;
+    }
+  } else {
+    // ipString is invalid
+    return "bad IP";
+  }
+}
 
 exports.SaveAPIRequest = function(ip,functionname,inputs){
 
+  var newIP = transformIP(ip);
+
+
   var newAPIRequest = new APIRequest ({
-    'ip' : ip,
+    'ip' : newIP,
     'function' : functionname,
     'inputs' : JSON.stringify(inputs)
   });
